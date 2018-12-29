a637c876b30fca57d6931cc1313966f1e8e080ed
poduck <poduck@gmail.com>
2018-12-28 22:55:52 -0500

    Changed help text.

--- lights/models.py
+++ lights/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Light(models.Model):
-    color = models.CharField(max_length=255)
-    pin = models.IntegerField()
-    delay = models.FloatField()
-    duration = models.FloatField()
+    color = models.CharField(max_length=255, help_text="This can be a color, or any description of the light, such as 'Yellow Arrow'")
+    pin = models.IntegerField(help_text="BCM mode pin number of the switch.  Not the physical pin number.")
+    delay = models.FloatField(help_text="Amount of time from the start of the cycle until this light turns on.")
+    duration = models.FloatField(help_text="Length of time this light stays on after it comes on.")
     pull = models.BooleanField(default=False, verbose_name="Pull high", help_text="Check the box if the light should be pulled high when it is on.")
