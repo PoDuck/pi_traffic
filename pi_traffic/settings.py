f4481f8134a2f15676632f01401d129992d35f3d
poduck <poduck@gmail.com>
2018-12-28 19:00:15 -0500

    Setup package.json and cleaned up links to static files so they work with imported files.

--- pi_traffic/settings.py
+++ pi_traffic/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'node_modules'),
+    os.path.join(BASE_DIR, 'static'),
+]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
